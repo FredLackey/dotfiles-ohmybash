f6338fed99478980cbcc4a3f8281f46123168c9d
Fred Lackey <fred.lackey@gmail.com>
2023-07-17 19:20:09 -0400

    initial tweaks

--- src/os/installs/macos/vscode.sh
+++ src/os/installs/macos/vscode.sh
@@ -23,7 +23,24 @@ printf "\n"
 install_plugin "EditorConfig" "EditorConfig.EditorConfig"
 install_plugin "File Icons" "vscode-icons-team.vscode-icons"
 install_plugin "MarkdownLint" "DavidAnson.vscode-markdownlint"
-install_plugin "Vim" "vscodevim.vim"
+# install_plugin "Vim" "vscodevim.vim"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_plugin "chouzz.vscode-better-align" "Better Align"
+install_plugin "dracula-theme.theme-dracula" "Darkula Official Theme"
+install_plugin "EditorConfig.EditorConfig" "ESLint Support"
+install_plugin "dsznajder.es7-react-js-snippets" "ES7 React Snippets"
+install_plugin "FerrierBenjamin.fold-unfold-all-icone" "Fold / Unfold All Icons"
+install_plugin "GitHub.copilot" "GitHub CoPilot"
+install_plugin "ritwickdey.LiveServer" "Live Server"
+install_plugin "johnpapa.vscode-peacock" "Peacock"
+install_plugin "esbenp.prettier-vscode" "Prettier"
+install_plugin "Prisma.prisma" "Prisma"
+install_plugin "vscode-icons-team.vscode-icons" "VSCode Icons"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 # Close VSCode
 osascript -e 'quit app "Visual Studio Code"'
--- src/os/preferences/macos/finder.sh
+++ src/os/preferences/macos/finder.sh
@@ -46,25 +46,25 @@ execute "defaults write com.apple.finder ShowRecentTags -bool false" \
 execute "defaults write -g AppleShowAllExtensions -bool true" \
     "Show all filename extensions"
 
-execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:iconSize 72' ~/Library/Preferences/com.apple.finder.plist && \
-         /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:iconSize 72' ~/Library/Preferences/com.apple.finder.plist" \
-    "Set icon size"
+# execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:iconSize 72' ~/Library/Preferences/com.apple.finder.plist && \
+#          /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:iconSize 72' ~/Library/Preferences/com.apple.finder.plist" \
+#     "Set icon size"
 
-execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:gridSpacing 1' ~/Library/Preferences/com.apple.finder.plist && \
-         /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:gridSpacing 1' ~/Library/Preferences/com.apple.finder.plist" \
-    "Set icon grid spacing size"
+# execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:gridSpacing 1' ~/Library/Preferences/com.apple.finder.plist && \
+#          /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:gridSpacing 1' ~/Library/Preferences/com.apple.finder.plist" \
+#     "Set icon grid spacing size"
 
-execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:textSize 13' ~/Library/Preferences/com.apple.finder.plist && \
-         /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:textSize 13' ~/Library/Preferences/com.apple.finder.plist" \
-    "Set icon label text size"
+# execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:textSize 13' ~/Library/Preferences/com.apple.finder.plist && \
+#          /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:textSize 13' ~/Library/Preferences/com.apple.finder.plist" \
+#     "Set icon label text size"
 
-execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:labelOnBottom true' ~/Library/Preferences/com.apple.finder.plist && \
-         /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:labelOnBottom true' ~/Library/Preferences/com.apple.finder.plist" \
-    "Set icon label position"
+# execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:labelOnBottom true' ~/Library/Preferences/com.apple.finder.plist && \
+#          /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:labelOnBottom true' ~/Library/Preferences/com.apple.finder.plist" \
+#     "Set icon label position"
 
-execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:showItemInfo true' ~/Library/Preferences/com.apple.finder.plist && \
-         /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:showItemInfo true' ~/Library/Preferences/com.apple.finder.plist" \
-    "Show item info"
+# execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:showItemInfo true' ~/Library/Preferences/com.apple.finder.plist && \
+#          /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:showItemInfo true' ~/Library/Preferences/com.apple.finder.plist" \
+#     "Show item info"
 
 execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:arrangeBy none' ~/Library/Preferences/com.apple.finder.plist && \
          /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:arrangeBy none' ~/Library/Preferences/com.apple.finder.plist" \
@@ -76,6 +76,6 @@ killall "Finder" &> /dev/null
 # so in order for things to get properly set using `PlistBuddy`,
 # the `cfprefsd` process also needs to be killed.
 #
-# https://github.com/alrra/dotfiles/commit/035dda057ddc6013ba21db3d2c30eeb51ba8f200
+# https://github.com/fredlackey/dotfiles/commit/035dda057ddc6013ba21db3d2c30eeb51ba8f200
 
 killall "cfprefsd" &> /dev/null
