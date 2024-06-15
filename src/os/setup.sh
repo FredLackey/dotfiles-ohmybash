3f99072dbb100f961bfe9fc1f7b81fd9ddf4cb17
Fred Lackey <fred.lackey@gmail.com>
2024-06-15 08:10:13 -0400

    add backup_bash_files

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -242,6 +242,10 @@ main() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    ./backup_bash_files.sh
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     # Check if this script was run directly (./<path>/setup.sh),
     # and if not, it most likely means that the dotfiles were not
     # yet set up, and they will need to be downloaded.
