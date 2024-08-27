f6d465d0c73a6cd8404039ef6d3044de0b56921c
Fred Lackey <fred.lackey@gmail.com>
2024-08-27 12:26:37 -0400

    correct macos go

--- src/os/installs/macos/misc_tools.sh
+++ src/os/installs/macos/misc_tools.sh
@@ -39,7 +39,7 @@ brew_install "Draw.IO" "drawio" "--cask"
 brew_install "Elmedia Player" "elmedia-player" "--cask"
 # brew_install "Evernote" "evernote" "--cask"
 
-brew_install "Go" "go" "--cask"
+brew_install "Go" "go"
 
 brew_install "Keyboard Maestro" "keyboard-maestro" "--cask"
 
