3d149eb8dcf65a79808271f455d5bbae8904bcc4
Fred Lackey <fred.lackey@gmail.com>
2024-11-14 11:25:37 -0500

    nvm install 20

--- src/os/installs/nvm.sh
+++ src/os/installs/nvm.sh
@@ -41,8 +41,8 @@ install_latest_stable_node() {
 
     execute \
         ". $LOCAL_SHELL_CONFIG_FILE \
-            && nvm install 18" \
-        "Install Node 18"
+            && nvm install 20" \
+        "Install Node 20"
 }
 
 install_nvm() {
