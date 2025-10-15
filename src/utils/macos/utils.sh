7273b78807f85324b52a6b6a8fa7dbbeafc0f887
Fred Lackey <fred.lackey@gmail.com>
2025-10-15 06:39:33 -0400

    xcode tools install (again)

--- src/utils/macos/utils.sh
+++ src/utils/macos/utils.sh
@@ -135,3 +135,36 @@ opt_out_of_homebrew_analytics() {
     fi
 
 }
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Xcode Command Line Tools
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+are_xcode_command_line_tools_installed() {
+    xcode-select --print-path &> /dev/null
+}
+
+install_xcode_command_line_tools() {
+
+    # If already installed, skip
+    if are_xcode_command_line_tools_installed; then
+        print_success "Xcode Command Line Tools already installed"
+        return 0
+    fi
+
+    # If necessary, prompt user to install
+    # the `Xcode Command Line Tools`.
+
+    xcode-select --install &> /dev/null
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait until the `Xcode Command Line Tools` are installed.
+
+    execute \
+        "until are_xcode_command_line_tools_installed; do \
+            sleep 5; \
+         done" \
+        "Install Xcode Command Line Tools"
+
+}
